7cce3259b20171e39f8126fa03873339e095e342
jordan <jordan52@gmail.com>
2015-01-28 15:58:51 -0600

    figuring out a high resolution timer for claps

--- app/claps/claps.js
+++ app/claps/claps.js
@@ -9,6 +9,36 @@
 var clapDelays = [89.07511089,  102.23344158,   99.40623555,  101.12356061,  117.28880854,
     125.3285453,    89.50526668,   72.32998573,   96.34697385,  107.97372603];
 var volumes = [0.3223254,   0.75143339,  0.47993786, 0.9172416,   0.58518288,0.3223254,   0.75143339,  0.47993786, 0.9172416,   0.58518288];
+
+//from https://gist.github.com/manast/1185904
+function interval(duration, fn){
+    this.baseline = undefined
+
+    this.run = function(){
+        if(this.baseline === undefined){
+            this.baseline = new Date().getTime()
+        }
+        fn()
+        var end = new Date().getTime()
+        this.baseline += duration
+
+        var nextTick = duration - (end - this.baseline)
+        if(nextTick<0){
+            nextTick = 0
+        }
+        (function(i){
+            i.timer = setTimeout(function(){
+                i.run(end)
+            }, nextTick)
+        }(this))
+    }
+
+    this.stop = function(){
+        clearTimeout(this.timer)
+    }
+}
+
+
 angular.module('myApp.claps', ['ngRoute'])
 
     .config(['$routeProvider', function ($routeProvider) {
@@ -20,6 +50,15 @@ angular.module('myApp.claps', ['ngRoute'])
 
     //ok, now use ideas from https://github.com/danielstern/ngAudio to finish.
     .controller('ClapsCtrl', ['$scope', function ($scope) {
+
+
+
+        $scope.timer = new interval(50, function(){
+            console.log(new Date().getTime())
+        })
+        //$scope.timer.run()
+
+
         $scope.claps = [];
         $scope.playing = false;
         for(var i = 0; i < clapDelays.length;i++){
